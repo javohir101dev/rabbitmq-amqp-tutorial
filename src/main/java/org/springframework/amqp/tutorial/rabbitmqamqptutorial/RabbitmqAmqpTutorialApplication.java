a3d446490f3c5ceb69b7eb4b02006a5bf8c8ed46
Javohir Uralov <uralovjavohir25@gmail.com>
2022-07-27 11:07:50 +0500

    Initial commit

---
+++ src/main/java/org/springframework/amqp/tutorial/rabbitmqamqptutorial/RabbitmqAmqpTutorialApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.amqp.tutorial.rabbitmqamqptutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAmqpTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqAmqpTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springframework/amqp/tutorial/rabbitmqamqptutorial/RabbitmqAmqpTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.amqp.tutorial.rabbitmqamqptutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqAmqpTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
